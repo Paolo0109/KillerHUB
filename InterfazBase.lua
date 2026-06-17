6cbee50d2f51afe8b9da5174350e411fdd794fc0
Paolo0109 <anonymous01110110101@gmail.com>
2026-06-17 17:13:25 -0600

    Actualizar InterfazBase.lua

--- InterfazBase.lua
+++ InterfazBase.lua
@@ -739,5 +739,29 @@ SettingsTab:CreateParagraph("⚠️ ADVERTENCIA DE APAGADO", "Si decides apagar 
 SettingsTab:CreateButton("Apagar Script por Completo (Unload)", function() KillerHub:Unload() end)
 
 getgenv().KillerHub = KillerHub
-warn("✅ KillerHub Universal cargado y arreglado perfectamente.")
+warn([[
+
+  _  _  _  _  _                     _    _         _       
+ | |/ / (_)| | |                   | |  | |       | |      
+ | ' /   _ | | |  ___  _ __        | |__| |_   _  | |__    
+ |  <   | || | | / _ \| '__|       |  __  | | | | | '_ \   
+ | . \  | || | ||  __/| |          | |  | | |_| | | |_) |  
+ |_|\_\ |_||_|_| \___||_|          |_|  |_|\__,_| |_.__/   
+                                                           
+                ____   __     __                           
+               |  _ \  \ \   / /                           
+               | |_) |  \ \_/ /                            
+               |  _ <    \   /                             
+               | |_) |    | |                              
+               |____/     |_|                              
+                                                           
+  _____                 _                                  
+ |  __ \               | |                                 
+ | |__) | __ _   ___   | |  ___                            
+ |  ___/ / _` | / _ \  | | / _ \                           
+ | |    | (_| || (_) | | || (_) |                          
+ |_|     \__,_| \___/  |_| \___/                           
+                                                           
+
+]])
 return KillerHub
